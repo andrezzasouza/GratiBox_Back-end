62f50df86906fbbb2980c3f264791ecfef9ad517
Andrezza Souza <andrezzasouza@id.uff.br>
2021-11-22 15:08:07 -0300

    Send data from database to the front-end

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import signUp from './controllers/sign-up.js';
 import login from './controllers/login.js';
 import placeOrder from './controllers/place-order.js';
+import details from './controllers/details.js';
 import checkToken from './middlewares/auth.js';
 
 const app = express();
@@ -12,8 +13,7 @@ app.use(express.json());
 
 app.post('/sign-up', signUp);
 app.post('/login', login);
-// add middleware here
 app.post('/place-order', checkToken, placeOrder);
-// app.get('/details', details);
+app.get('/details', checkToken, details);
 
 export default app;
