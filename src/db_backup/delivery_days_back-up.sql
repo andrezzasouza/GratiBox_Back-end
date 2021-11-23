582678b03c66ce081a9ab73d6bd8cd0ba49add64
Matheus Galdino <matheus@respondeai.com.br>
2021-11-23 12:20:33 -0300

    preparando o review

--- src/db_backup/delivery_days_back-up.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO delivery_days (day) VALUES ('Segunda');
-INSERT INTO delivery_days (day) VALUES ('Quarta');
-INSERT INTO delivery_days (day) VALUES ('Sexta');
-INSERT INTO delivery_days (day) VALUES ('Dia 01');
-INSERT INTO delivery_days (day) VALUES ('Dia 10');
-INSERT INTO delivery_days (day) VALUES ('Dia 20');
--- src/db_backup/products_back-up.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO products (name) VALUES ('Chá');
-INSERT INTO products (name) VALUES ('Incenso');
-INSERT INTO products (name) VALUES ('Produtos orgânicos');
